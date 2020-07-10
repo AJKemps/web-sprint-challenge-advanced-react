92d2f15f94792c85de575bf59d3a25c5cb8dc9f8
Alex Kemper <alexkemper@MacBook-Pro.local>
2020-07-10 12:35:43 -0400

    tests complete

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -12,8 +12,26 @@ test("form header renders", () => {
   expect(header).toBeInTheDocument();
 });
 
-test("form shows success message on submit with form details", () => {
+test("form shows success message on submit with form details", async () => {
   render(<CheckoutForm />);
 
-  const firstNameInput = screen.getByText(/First Name/i);
+  const firstNameInput = screen.getByLabelText(/First Name/i);
+  const lastNameInput = screen.getByLabelText(/Last Name/i);
+  const addressInput = screen.getByLabelText(/address/i);
+  const cityInput = screen.getByLabelText(/city/i);
+  const stateInput = screen.getByLabelText(/state/i);
+  const zipInput = screen.getByLabelText(/zip/i);
+  const submitButton = screen.getByRole("button");
+
+  fireEvent.change(firstNameInput, { target: { value: "Bob" } });
+  fireEvent.change(lastNameInput, { target: { value: "Bobertson" } });
+  fireEvent.change(addressInput, { target: { value: "123 Oak St" } });
+  fireEvent.change(cityInput, { target: { value: "Milwaukee" } });
+  fireEvent.change(stateInput, { target: { value: "Wisconsin" } });
+  fireEvent.change(zipInput, { target: { value: "43210" } });
+  fireEvent.click(submitButton);
+
+  const successMessage = await screen.findByTestId("successMessage");
+
+  expect(successMessage).toBeInTheDocument();
 });
